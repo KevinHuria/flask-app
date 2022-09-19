93d90e1c30636d50c854df0ec451dfa7a694f5ab
Kevin Huria <huriakevin@gmail.com>
2022-09-19 12:17:21 +0300

    Added changes to forms.py

--- app/app/forms.py
+++ app/app/forms.py
@@ -1,5 +1,5 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField,SubmitField, PasswordField
+from wtforms import StringField,SubmitField, PasswordField, BooleanField
 from wtforms.validators import DataRequired, Length,Email,EqualTo
 
 class SignUpform(FlaskForm):
@@ -16,7 +16,20 @@ class SignUpform(FlaskForm):
                             validators=[DataRequired(),Length(min=8)])
     confirm_password=PasswordField('Password',
                                     validators=[DataRequired(),EqualTo('password')])
+    submit=SubmitField('Sign Up')
 
+
+class LogInform(FlaskForm):
+    email = StringField(
+        'Email', validators=[
+            Email(message=("Not a valid email")),
+            DataRequired()
+        ]
+    )
+    password = PasswordField('Password',
+                             validators=[DataRequired(), Length(min=8)])
+    remember=BooleanField('Remember Me')
+    submit = SubmitField('LogIn')
 
 
 
