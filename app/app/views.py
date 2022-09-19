7771f8d236f0e93d62d568d0e70b9f1e1070775a
Kevin Huria <huriakevin@gmail.com>
2022-09-19 10:24:15 +0300

    Added changes to the views.py page

--- app/app/views.py
+++ app/app/views.py
@@ -1,22 +1,36 @@
+from crypt import methods
+from wsgiref.util import request_uri
 from app import app
 
-from flask import render_template
+from flask import render_template, request, redirect
+
 
 @app.route("/")
 def index():
     return render_template("public/index.html")
 
+
 @app.route("/jinja")
 def jinja():
 
-    my_name= 'Kevin'
-    return render_template("public/jinja.html",my_name=my_name)
+    my_name = 'Kevin'
+    return render_template("public/jinja.html", my_name=my_name)
 
 
 @app.route("/about")
 def about():
     return "<h1>About!</h1>"
 
-@app.route("/sign-up")
+
+@app.route("/sign-up", methods=["GET", "POST"])
 def sign_up():
+    if request.method == "POST":
+        req = request.form
+        username = req["username"]
+        email=req.get("email")
+        password=request.form["password"]
+
+        print(username, email, password)
+
+        return redirect(request.url)
     return render_template("public/sign_up.html")
