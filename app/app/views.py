067662af9f7ae2cf8628e99a3420935309299528
Kevin Huria <huriakevin@gmail.com>
2022-10-07 13:17:54 +0300

    added changes to login and signup

--- app/app/views.py
+++ app/app/views.py
@@ -1,6 +1,9 @@
 from app import app
-from flask import render_template, url_for, redirect, flash
+from flask import render_template, url_for, redirect, flash, request
 from app.forms import SignUpform, LogInform
+from app.models import User
+from app import app, db, pwd
+from flask_login import login_user, current_user, logout_user, login_required
 
 app.config['SECRET_KEY'] = '19a27057ac27ad42c9f047c62aeb6f15'
 
@@ -10,13 +13,6 @@ def index():
     return render_template("public/index.html")
 
 
-@app.route("/jinja")
-def jinja():
-
-    my_name = 'Kevin'
-    return render_template("public/jinja.html", my_name=my_name)
-
-
 @app.route('/about')
 def about():
     return "<h1>About!</h1>"
@@ -24,26 +20,47 @@ def about():
 
 @app.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-    form = SignUpform()
-    if form.validate_on_submit():
-        flash(f'Account created for {form.username.data}!','success')
-        ##Displays flash message upon form validation
+    if current_user.is_authenticated:
+        flash("You are already logged in.", "warning")
+        return redirect(url_for('index'))
+    form = SignUpform(request.form)
+    if request.method == "POST" and form.validate():
+        hashed = pwd.generate_password_hash(form.password.data).decode('utf-8')
+        element = User(uname=form.username.data,
+                       email=form.email.data, password=hashed)
+        db.session.add(element)
+        db.session.commit()
+        flash("Account created for %s!" % (form.username.data), "success")
+        # Displays flash message upon form validation
         return redirect(url_for('index'))
     return render_template("public/sign_up.html", form=form)
 
 
-@app.route('/logIn', methods=['GET', 'POST'])
+@ app.route('/logIn', methods=['GET', 'POST'])
 def LogIn():
-    form = LogInform()
-    if form.validate_on_submit():
-        if form.email.data=="huria@gmail.com" and form.password.data=="password":
-            flash("You have logged in!",'success')
+    if current_user.is_authenticated:
+        flash("You are already logged in.", "warning")
+        return redirect(url_for('index'))
+        form = LogInForm(request.form)
+        if request.method == "POST" and form.validate():
+          user = User.query.filter_by(uname=form.username.data).first()
+        if user and pwd.check_password_hash(user.password, form.password.data):
+            login_user(user)
+            flash("Welcome, %s!" % (form.username.data), "success")
             return redirect(url_for('index'))
-        else:
-            flash("LogIn Unsuccessful",'danger')
+    else:
+        flash("LogIn Unsuccessful", 'danger')
+        return redirect(url_for('login'))
     return render_template('public/login.html', form=form)
 
 
-@app.route("/profile/<username>")
+@app.route("/logout")
+def logoutpage():
+    logout_user()
+    flash("Successfuly logged out.", "success")
+    return redirect(url_for('index'))
+
+
+@ app.route("/profile/<username>")
 def profile(username):
     return render_template("public/profile.html")
